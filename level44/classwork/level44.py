eded48c7cc82d620f750d1e515ad58174c1a95b6
lukatomo <postman.alfrer@gmail.com>
2025-03-26 16:52:01 +0400

    level45

---
+++ level44/classwork/level44.py
@@ -0,0 +1,13 @@
+# # min() ფუნქციის გამოყენებით:
+
+#  def find_min(lst):
+#     return min(lst)
+# # მინიმუმის მოძიება ხელით:
+
+
+# def find_min_manual(lst):
+#     min_val = lst[0]  # დასაწყისი - პირველი ელემენტი
+#    for num in lst:
+#       if num < min_val:
+#            min_val = num
+#   return min_val
