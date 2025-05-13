eb12708a6504a909d97a9bd203cbdcdb56bdd0a5
lukatomo <postman.alfrer@gmail.com>
2025-05-13 20:46:37 +0400

    level52

---
+++ level52/claswork/level52.js
@@ -0,0 +1,5 @@
+// პარაგრაფის ტექსტის შეცვლა
+document.getElementById("myParagraph").innerText = "პარაგრაფი შეიცვალა JavaScript-ის მეშვეობით.";
+
+// ღილაკის ტექსტის შეცვლა
+document.getElementById("myButton").innerText = "ღილაკის ტექსტიც შეიცვალა!";
