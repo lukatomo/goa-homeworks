a8148db83aada19473cfb82e464a66e96f64923f
lukatomo <postman.alfrer@gmail.com>
2026-01-17 22:44:02 +0400

    commit\

--- js.js
+++
@@ -1,8 +0,0 @@
-setInterval(addOne, 1000);
-
-function addOne(){
-  let count = document.getElementById("count");
-  number = Number(count.textContent);
-  number = number + 1;
-  count.textContent = number;
-}
--- lukasixarulidze.py
+++
@@ -1,8 +0,0 @@
-word = "lUkA"
-print(word.capitalize())
-print(word.lower())
-print(word.upper())
-print(word.swapcase())
-print(word.strip())
-print(word.find("A"))
-print(word)
